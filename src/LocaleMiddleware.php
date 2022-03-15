3f0db7921ec5b1bd2cdd70e2b094603f36dafdaf
郭靖 <>
2022-03-16 01:46:44 +0800

    变更

---
+++ src/LocaleMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Zikix\LaravelComponent;
+
+use Closure;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+
+/**
+ * Class LocaleMiddleware
+ */
+class LocaleMiddleware
+{
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @param null    $guard
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    public function handle(Request $request, Closure $next, $guard = null)
+    {
+        $language = $request->input('language');
+        if (!$language) {
+            // zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7
+            $language = $request->header('Accept-Language');
+            if (strpos($language, 'zh-CN') === 0) {
+                $language = 'zh';
+            }
+            if (strpos($language, 'en-US') === 0) {
+                $language = 'en';
+            }
+        }
+
+        if (!$language) {
+            $language = config('app.locale');
+        }
+
+        App::setLocale($language);
+
+        return $next($request);
+    }
+}
