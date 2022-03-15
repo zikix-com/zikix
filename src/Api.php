68ce3191257e671ef0074d0ad6ad15d762c8c2be
郭靖 <>
2022-03-16 06:27:06 +0800

    变更

--- src/Api.php
+++ src/Api.php
@@ -74,7 +74,7 @@ class Api
         $headers['Access-Control-Allow-Credentials'] = 'true';
         $headers['Access-Control-Allow-Methods']     = '*';
         $headers['Access-Control-Expose-Headers']    = '*';
-        $headers['Access-Control-Allow-Headers']     = 'Content-Type,Access-Token,AUTHORIZATION,X-REQUESTED-WITH';
+        $headers['Access-Control-Allow-Headers']     = '*';
 
         return new JsonResponse($content, $httpCode, $headers, $options);
     }
