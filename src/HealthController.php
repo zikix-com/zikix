f860ffc8579b548a3517e6bc342a95e5a80991a4
郭靖 <>
2022-03-20 08:13:44 +0800

    add healthy

---
+++ src/HealthController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Zikix\LaravelComponent;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Routing\Controller as BaseController;
+
+class HealthController extends BaseController
+{
+    /**
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function __invoke()
+    {
+        return Api::ok();
+    }
+}
