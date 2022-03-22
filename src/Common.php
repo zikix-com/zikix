e4aa3f0f9b90dfc7520c7142190b16f74faf0017
郭靖 <>
2022-03-22 08:45:26 +0800

    update

--- src/Common.php
+++ src/Common.php
@@ -4,6 +4,7 @@ namespace Zikix\Component;
 
 use Exception;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Http;
 use Throwable;
 
 class Common
@@ -34,5 +35,22 @@ class Common
 
         return $exception;
 
+    }
+
+    /**
+     * @param string $ip
+     *
+     * @return array|mixed
+     */
+    public static function ip(string $ip)
+    {
+        try {
+            $data = Http::timeout(1)
+                        ->get("http://opendata.baidu.com/api.php?query=$ip&co=&resource_id=6006&oe=utf8");
+        } catch (Exception $exception) {
+            return [];
+        }
+
+        return $data['data'] ?? [];
     }
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
-if (!ini_get('date.timezone')) {
-    date_default_timezone_set('GMT');
-}
+ini_set('date.timezone', 'Asia/Shanghai');
 
 $loader = require dirname(__DIR__) . '/vendor/autoload.php';
 $loader->add('Zikix\\LaravelComponent\\Test\\', __DIR__);
