acfc926b06158c89a9cf01935d930f34871c4f21
郭靖 <>
2022-03-31 18:14:36 +0800

    array_change_key_case_recursive

--- src/helpers.php
+++ src/helpers.php
@@ -5,13 +5,33 @@ use Zikix\Component\Api;
 
 if (!function_exists('zikix')) {
     /**
-     * @param mixed $data
+     * @param $array
+     * @param int $case
+     *
+     * @return array
+     */
+    function array_change_key_case_recursive(&$array, int $case = CASE_LOWER)
+    {
+        $array = array_change_key_case($array, $case);
+        foreach ($array as $key => $value) {
+            if (is_array($value)) {
+                array_change_key_case_recursive($array[$key], $case);
+            }
+        }
+
+        return $array;
+    }
+}
+
+if (!function_exists('zikix')) {
+    /**
+     * @param mixed|array|object $data
      * @param string $message
      *
      * @return JsonResponse
      * @throws Exception
      */
-    function zikix($data = [], string $message = '')
+    function zikix($data = [], string $message = ''): JsonResponse
     {
         return Api::ok($data, $message);
     }
