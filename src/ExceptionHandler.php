926768227eddbf33f31b69bb08e9605bceb5e8cd
z <z@z.com>
2022-04-09 00:04:38 +0800

    sls

--- src/ExceptionHandler.php
+++ src/ExceptionHandler.php
@@ -2,9 +2,12 @@
 
 namespace Zikix\Zikix;
 
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
@@ -46,27 +49,42 @@ class ExceptionHandler extends Handler
         $this->renderable(function (Throwable $e, Request $request) {
 
             if ($request->ajax() || $request->isJson()) {
+                return self::api($e, $request);
+            }
 
-                if ($e instanceof HttpResponseException) {
-                    return $e->getResponse();
-                }
-
-                $message = '服务器繁忙';
-                $data    = [];
-
-                if ($e instanceof NotFoundHttpException) {
-                    $message = "请求的API地址不存在: {$request->getMethod()} {$request->url()}";
-                }
-
-                if (!Common::isProduction()) {
-                    $message           = $e->getMessage() ?: $message;
-                    $data['exception'] = Common::exceptionToArray($e, true);
-                }
-
-                return Api::response(500, $message, $data, 500);
+            if ($request->route() && in_array('api', $request->route()->middleware(), true)) {
+                return self::api($e, $request);
             }
 
         });
 
+    }
+
+    /**
+     * @param Throwable $e
+     * @param Request $request
+     *
+     * @return JsonResponse|Response
+     * @throws Exception
+     */
+    private static function api(Throwable $e, Request $request)
+    {
+        if ($e instanceof HttpResponseException) {
+            return $e->getResponse();
+        }
+
+        $message = '服务器繁忙';
+        $data    = [];
+
+        if ($e instanceof NotFoundHttpException) {
+            $message = "请求的API地址不存在: {$request->getMethod()} {$request->url()}";
+        }
+
+        if (!Common::isProduction()) {
+            $message           = $e->getMessage() ?: $message;
+            $data['exception'] = Common::exceptionToArray($e, true);
+        }
+
+        return Api::response(500, $message, $data, 500);
     }
 }
