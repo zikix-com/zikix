08050d7f7c6cb5bd68dba17f635cb2d27d6f0c16
Bean <bean@com.com>
2022-07-16 18:26:32 +0800

    improved query

--- src/Api.php
+++ src/Api.php
@@ -128,7 +128,7 @@ class Api
      *
      * @return void
      */
-    public static function setRequest(Request $request): void
+    public static function setRequest(Request $request = null): void
     {
         self::$request = $request;
     }
