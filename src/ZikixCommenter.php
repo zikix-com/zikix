d4ffab6a6f85b2a3ecfeb2f36c38f3edbbfa722f
Bean <bean@com.com>
2022-07-16 12:55:48 +0800

    add debug user

--- src/ZikixCommenter.php
+++ src/ZikixCommenter.php
@@ -2,9 +2,7 @@
 
 namespace Zikix\Zikix;
 
-use Closure;
 use Illuminate\Database\Connection;
-use Laravel\SerializableClosure\Support\ReflectionClosure;
 use Spatie\SqlCommenter\Comment;
 use Spatie\SqlCommenter\Commenters\Commenter;
 
@@ -20,39 +18,9 @@ class ZikixCommenter implements Commenter
      */
     public function comments(string $query, Connection $connection): Comment|array|null
     {
-
-        [$controller, $action] = $this->getControllerAndAction();
-
         return [
             Comment::make('rid', Api::getRequestId()),
-            Comment::make('controller', $controller),
         ];
-    }
-
-    /**
-     * @return array
-     */
-    protected function getControllerAndAction(): array
-    {
-        $action = request()->route()->getAction('uses');
-
-        if ($action instanceof Closure) {
-            $reflection = new ReflectionClosure($action);
-            $controller = 'Closure';
-            $action     = $reflection->getFileName();
-
-            return [$controller, $action];
-        }
-
-        $controller = explode('@', $action)[0] ?? null;
-
-        if (!$this->includeNamespace) {
-            $controller = class_basename($controller);
-        }
-
-        $action = explode('@', $action)[1] ?? null;
-
-        return [$controller, $action];
     }
 
 }
